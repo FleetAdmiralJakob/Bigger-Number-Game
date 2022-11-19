1953b4d2571753a08c05cfa91520cd411545b95c
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-11-19 22:32:00 +0100

    .

--- app/src/main/java/com/jakob/biggernumbergame/MainActivity.kt
+++ app/src/main/java/com/jakob/biggernumbergame/MainActivity.kt
@@ -1,22 +1,72 @@
 package com.jakob.biggernumbergame
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    var AcScore = 0
+    var AcHighscore = 0
+    var language = "English"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Main
         val btnRight = findViewById<Button>(R.id.btnRight)
         val btnLeft = findViewById<Button>(R.id.btnLeft)
+        val tvAcScore = findViewById<TextView>(R.id.tvAcScore)
+        val tvAcHighscore = findViewById<TextView>(R.id.tvAcHighscore)
+        val languageSpinner = findViewById<Spinner>(R.id.languageSpinner)
 
-        assignNumbersToButtons()
+        // For Translation
+        val title = findViewById<TextView>(R.id.title)
+        val description = findViewById<TextView>(R.id.description)
+        val tvScore = findViewById<TextView>(R.id.tvScore)
+        val tvHighscore = findViewById<TextView>(R.id.tvHighscore)
+        val tvlanguage = findViewById<TextView>(R.id.tvlanguage)
+
+        val sharedPrefHigh = getSharedPreferences("highscore", Context.MODE_PRIVATE)
+        AcHighscore = sharedPrefHigh.getInt("score", 0)
+
+        tvAcScore.text = "$AcScore"
+        tvAcHighscore.text = "$AcHighscore"
+
+        languageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: android.view.View,
+                pos: Int,
+                id: Long
+            ) {
+                if (language == "English") {
+                    title.text = getString(R.string.title)
+                    description.text = getString(R.string.description)
+                    tvlanguage.text = getString(R.string.language)
+                } else if (language == "German") {
+                    title.text = getString(R.string.title_de)
+                    description.text = getString(R.string.description_de)
+                    tvlanguage.text = getString(R.string.language_de)
+                }
+                when (languageSpinner.selectedItemPosition) {
+                    0 -> language = "German"
+                    1 -> language = "English"
+                }
+            }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+        }
+
+            assignNumbersToButtons()
 
         btnLeft.setOnClickListener {
             checkAnwser(true)
@@ -35,6 +85,8 @@ class MainActivity : AppCompatActivity() {
         val backgroundView = findViewById<ConstraintLayout>(R.id.backgroundView)
         val btnRight = findViewById<Button>(R.id.btnRight)
         val btnLeft = findViewById<Button>(R.id.btnLeft)
+        val tvAcScore = findViewById<TextView>(R.id.tvAcScore)
+        val tvAcHighscore = findViewById<TextView>(R.id.tvAcHighscore)
 
         val leftNum: Int = btnLeft.text.toString().toInt()
         val rightNum: Int = btnRight.text.toString().toInt()
@@ -51,12 +103,27 @@ class MainActivity : AppCompatActivity() {
             backgroundView.setBackgroundColor(Color.GREEN)
             //Show a toast
             Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            AcScore++
+            tvAcScore.text = "$AcScore"
+            if (AcScore > AcHighscore) {
+                AcHighscore = AcScore
+                tvAcHighscore.text = "$AcHighscore"
+                val sharedPrefHigh = getSharedPreferences("highscore", Context.MODE_PRIVATE)
+                val sharedPrefEdiHigh = sharedPrefHigh.edit()
+
+                sharedPrefEdiHigh.apply {
+                    putInt("score", AcHighscore)
+                    apply()
+                }
+            }
         } else {
             // Wrong answer!!
             // Change background color
             backgroundView.setBackgroundColor(Color.RED)
             //Show a toast
             Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
+            AcScore = 0
+            tvAcScore.text = "$AcScore"
         }
     }
 
